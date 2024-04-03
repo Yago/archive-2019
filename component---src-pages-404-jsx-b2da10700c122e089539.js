380849b9af7a7611ec513e22c157704ee4e7b3a6
Yann Gouffon <github@yago-art.ch>
2024-04-03 09:21:34 +0200

    add archive

---
+++ component---src-pages-404-jsx-b2da10700c122e089539.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{pssB:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),s=a.n(n),l=a("L12J"),u=a("dmsj");t.default=()=>s.a.createElement(l.a,null,s.a.createElement(u.a,{title:"404: Not found"}),s.a.createElement("h1",null,"NOT FOUND"),s.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-b2da10700c122e089539.js.map
